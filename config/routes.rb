ce338775aa72a413c834b7614a19fddd54011dc6
Yoshitaka Ide <yoshi4363@gmail.com>
2025-02-13 21:36:42 +0900

    chore: 開発環境から送信したメールをブラウザから確認可能に #25

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "User", at: "auth"
     end
   end
+
+  # 開発環境で送信したメールをブラウザから一覧するためのパス
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
