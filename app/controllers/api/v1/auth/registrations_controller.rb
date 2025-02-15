c38550470e27c8ba4fde2801f4cb756511d482f0
Yoshitaka Ide <yoshi4363@gmail.com>
2025-02-15 19:29:02 +0900

    feat: ユーザーのアカウント退会処理を追加 #29

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+        # DELETE /api/v1/auth
+        def destroy
+          if current_api_user
+            current_api_user.update(is_deleted: true, confirmed_at: nil, tokens: {})
+            render json: { message: "退会処理が正常に完了しました。" }, status: :ok
+          else
+            render json: { message: ["ユーザーが見つかりません。"] }, status: :not_found
+          end
+        end
+      end
+    end
+  end
+end
