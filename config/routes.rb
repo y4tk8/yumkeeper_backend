22e2b45c6a3368b647024ebe84f4aa5e0c0e099e
Yoshitaka Ide <yoshi4363@gmail.com>
2025-02-22 20:20:02 +0900

    feat: Recipesコントローラへのルーティング追加 #43

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    scope :v1 do
+    namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
         passwords:     "api/v1/auth/passwords"
       }
+      resources :recipes, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
