83d7230eafd143279239cf4cfa1ce653800705a0
Yoshitaka Ide <yoshi4363@gmail.com>
2025-02-22 14:18:05 +0900

    chore: Recipeのマイグレーションファイル作成と実行 #41

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_16_065021) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_21_162719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", limit: 100, null: false
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -43,4 +52,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_16_065021) do
     t.check_constraint "confirmed_at IS NULL OR confirmed_at >= confirmation_sent_at", name: "check_confirmation_times"
     t.check_constraint "role::text = ANY (ARRAY['一般'::character varying, '管理者'::character varying, 'ゲスト'::character varying]::text[])", name: "check_role"
   end
+
+  add_foreign_key "recipes", "users"
 end
