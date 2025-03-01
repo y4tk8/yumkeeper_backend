73901036c657cc8dfa53b657daa620f5c8235cc6
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-01 23:49:41 +0900

    feat: レシピに材料をネストして関連付けるよう設定を追加 #60

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :ingredients, dependent: :destroy
+  accepts_nested_attributes_for :ingredients, allow_destroy: true
 
   validates :name, presence: true, length: { maximum: 100 }
 end
