a84afa3309d8a30f4039c581cefd1feefad1bf7c
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-01 23:53:45 +0900

    chore: 材料・調味料のシードデータ生成コードを追加 #60

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,14 @@ users = FactoryBot.create_list(:user, 5, :confirmed)
 
 # 各ユーザーに5つのレシピを作成
 users.each do |user|
-  FactoryBot.create_list(:recipe, 5, user: user)
+  recipes = FactoryBot.create_list(:recipe, 5, user: user)
+
+  # 各レシピに5つの材料 or 調味料を作成
+  recipes.each do |recipe|
+    5.times do
+      FactoryBot.create(:ingredient, recipe: recipe)
+    end
+  end
 end
 
 puts "Seed data created successfully!"
