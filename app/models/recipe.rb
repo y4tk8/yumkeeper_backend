cf9c0cc15f541fe617fddc7c77a67471ac07957f
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-06 19:11:52 +0900

    feat: RecipeとVideoを関連付け #70

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :ingredients, dependent: :destroy
+  has_many   :ingredients, dependent: :destroy
+  has_one    :video,       dependent: :destroy
   accepts_nested_attributes_for :ingredients, allow_destroy: true
+  accepts_nested_attributes_for :video,       allow_destroy: true
 
   validates :name, presence: true, length: { maximum: 100 }
 end
