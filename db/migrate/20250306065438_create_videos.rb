0f6573cdc461b418e0446fbb52654e68b5875621
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-06 16:13:31 +0900

    chore: Videoモデルのマイグレーションファイル編集と実行 #66

---
+++ db/migrate/20250306065438_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[7.2]
+  def change
+    create_table :videos do |t|
+      t.references :recipe, null: false, foreign_key: true
+      t.text :thumbnail, null: false
+      t.column :status, :video_status, null: false, default: "public"
+      t.boolean :is_embeddable, null: false, default: true
+      t.boolean :is_deleted, null: false, default: false
+      t.timestamp :cached_at
+      t.timestamp :created_at, null: false
+    end
+  end
+end
