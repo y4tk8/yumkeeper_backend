3becf4aa713c0a77cae15711980ad31f5cc2f8a5
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-09 23:41:20 +0900

    feat: Videosコントローラを定義 #79

---
+++ app/controllers/api/v1/videos_controller.rb
@@ -0,0 +1,42 @@
+module Api
+  module V1
+    class VideosController < ApplicationController
+      # モデル名をキーにJSONデータを自動ラップする機能をオフに
+      wrap_parameters false
+
+      before_action :authenticate_api_v1_user! # Devise Token Authでユーザーのサインインを必須に
+      before_action :set_video
+      before_action :authorize_user
+
+      def update
+        if @video.update(video_params)
+          render json: { message: "動画が更新されました", video: @video }, status: :ok
+        else
+          render json: { error: "動画の更新に失敗しました", details: @video.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      # レシピページに埋め込む動画を取得する
+      def set_video
+        @video = Video.find_by(id: params[:id])
+
+        unless @video
+          render json: { error: "動画が見つかりません。" }, status: :not_found
+        end
+      end
+
+      # サインインユーザーと動画保持ユーザーが同一か検証する
+      def authorize_user
+        unless current_api_v1_user == @video.recipe.user
+          render json: { error: "アクセス権限がありません。" }, status: :forbidden
+        end
+      end
+
+      def video_params
+        params.require(:video).permit(:etag, :status, :is_embeddable, :is_deleted, :cached_at)
+      end
+    end
+  end
+end
