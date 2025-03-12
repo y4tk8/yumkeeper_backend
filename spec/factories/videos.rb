8aee63205a3e503d5e4750e5300fa982379d9a54
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-12 18:22:00 +0900

    refactor: Videosテーブルのカラム名をthumbnail_urlに変更 #89

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :video do
     video_id { "abcd1234XYZ" } # 仮のYouTube Video ID
     etag { "etag_sample_123" } # 仮のETag
-    thumbnail { "https://example.com/thumbnail.jpg " }
+    thumbnail_url { "https://example.com/thumbnail.jpg " }
     status { "public" }
     is_embeddable { true }
     is_deleted { false }
