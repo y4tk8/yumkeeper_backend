3a3af7d3710a74c0a4e9b3295aa2df366ede80f9
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-17 22:01:27 +0900

    feat: ユーザー論理削除にて関連レシピも全削除するよう追記 #103

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -6,10 +6,17 @@ module Api
         # DELETE /api/v1/auth
         def destroy
           if current_api_v1_user
-            current_api_v1_user.update(is_deleted: true, confirmed_at: nil, tokens: {})
-            render json: { message: "退会処理が正常に完了しました。" }, status: :ok
+            begin
+              ActiveRecord::Base.transaction do
+                current_api_v1_user.delete_recipes
+                current_api_v1_user.update!(is_deleted: true, confirmed_at: nil, tokens: {})
+              end
+              render json: { message: "退会処理が正常に完了しました。" }, status: :ok
+            rescue => e
+              render json: { error: "退会処理に失敗しました。" }, status: :internal_server_error
+            end
           else
-            render json: { message: ["ユーザーが見つかりません。"] }, status: :not_found
+            render json: { error: "ユーザーが見つかりません。" }, status: :not_found
           end
         end
       end
