a424370feb87a94148f326c45f70c7d41067aa80
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-17 16:10:14 +0900

    test: アカウント認証メール再送のリクエストスペックを追加 #101

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -220,4 +220,57 @@ RSpec.describe "User Authentication", type: :request do
       end
     end
   end
+
+  # アカウント認証メールを再送信
+  describe "POST /api/v1/auth/confirmation" do
+    context "未認証ユーザーがリクエストした場合" do
+      let(:unconfirmed_user) { create(:user, confirmed_at: nil) }
+
+      it "認証メールが再送され、ステータス200が返る" do
+        expect {
+          post "/api/v1/auth/confirmation", params: { email: unconfirmed_user.email, redirect_url: "http://frontend.example.com/confirmation" }
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body["message"]).to include("アカウント認証メールを再送しました。メールをご確認ください。")
+      end
+    end
+
+    context "認証済みユーザーがリクエストした場合" do
+      let(:user) { create(:user, :confirmed) }
+
+      it "認証メールが送信され、ステータス200が返る（セキュリティ観点）" do
+        expect {
+          post "/api/v1/auth/confirmation", params: { email: user.email, redirect_url: "http://frontend.example.com/confirmation" }
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body["message"]).to include("アカウント認証メールを再送しました。メールをご確認ください。")
+      end
+    end
+
+    context "退会済みユーザーがリクエストした場合" do
+      let(:deleted_user) { create(:user, :deleted, confirmed_at: nil) }
+
+      it "認証メールが送信され、ステータス200が返る（セキュリティ観点）" do
+        expect {
+          post "/api/v1/auth/confirmation", params: { email: deleted_user.email, redirect: "http://frontend.example.com/confirmation" }
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body["message"]).to include("アカウント認証メールを再送しました。メールをご確認ください。")
+      end
+    end
+
+    context "存在しないメールアドレスを指定した場合" do
+      it "認証メールは送信されず、ステータス200が返る" do
+        expect {
+          post "/api/v1/auth/confirmation", params: { email: "nonexistent@example.com", redirect: "http://frontend.example.com/confirmation" }
+        }.not_to change { ActionMailer::Base.deliveries.count }
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body["message"]).to include("アカウント認証メールを再送しました。メールをご確認ください。")
+      end
+    end
+  end
 end
