7c7c83999bfafe5133c98e993b2b16e48fb9ecef
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-19 01:11:19 +0900

    chore: マイグレーションを実行 #98

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_17_041319) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_18_051532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "video_status", ["public", "private", "unlisted"]
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.bigint "recipe_id", null: false
@@ -51,7 +79,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_17_041319) do
     t.datetime "confirmation_sent_at"
     t.string "email", null: false
     t.string "username"
-    t.string "profile_image_url"
     t.boolean "is_deleted", default: false
     t.string "role", default: "一般"
     t.datetime "last_login_at"
@@ -85,6 +112,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_17_041319) do
     t.index ["video_id"], name: "index_videos_on_video_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "ingredients", "recipes", on_delete: :cascade
   add_foreign_key "recipes", "users"
   add_foreign_key "videos", "recipes"
