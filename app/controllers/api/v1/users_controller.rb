06efc3d9261bbf955455249b2baca4762e47c2c9
Yoshitaka Ide <yoshi4363@gmail.com>
2025-03-19 01:18:18 +0900

    feat: ユーザープロフィールの取得、更新、削除のアクションを追加 #98

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,69 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      # モデル名をキーにJSONデータを自動ラップする機能をオフに
+      wrap_parameters false
+
+      before_action :authenticate_api_v1_user! # Devise Token Authでユーザーのサインインを必須に
+      before_action :set_user
+
+      def show
+        render json: {
+          user: {
+            id: @user.id,
+            username: @user.username || "未設定",
+            profile_image_url: @user.profile_image_url
+          }
+        }, status: :ok
+      end
+
+      def update
+        if params[:profile_image].present?
+          @user.profile_image.purge if @user.profile_image.attached? # 既存画像を削除
+          @user.profile_image.attach(params[:profile_image]) # 新しい画像をアップロード
+        end
+
+        if @user.update(user_params)
+          render json: {
+            message: "プロフィールを更新しました",
+            user: {
+              id: @user.id,
+              username: @user.username || "未設定",
+              profile_image_url: @user.profile_image_url
+            }
+          }, status: :ok
+        else
+          render json: { error: "プロフィールの更新に失敗しました", details: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def delete_profile_image
+        if @user.profile_image.attached?
+          @user.profile_image.purge
+          render json: { message: "プロフィール画像を削除しました", profile_image_url: @user.profile_image_url }, status: :ok
+        else
+          render json: { error: "プロフィール画像が設定されていません" }, status: :not_found
+        end
+      end
+
+      private
+
+      # ユーザーを取得する
+      def set_user
+        param_user = User.find_by(id: params[:id])
+
+        # サインインユーザーとパラメータ指定ユーザーが一致するか検証
+        if current_api_v1_user == param_user
+          @user = param_user
+        else
+          render json: { error: "アクセス権限がありません。" }, status: :forbidden
+        end
+      end
+
+      # ストロングパラメータ
+      def user_params
+        params.require(:user).permit(:username, :profile_image)
+      end
+    end
+  end
+end
