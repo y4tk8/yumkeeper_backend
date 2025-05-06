cec75ffda813706a469472789bf39e9cfdd74f65
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-06 19:53:24 +0900

    fix: アカウント削除のレスポンス文を'退会処理' -> '退会'に修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -21,16 +21,16 @@ module Api
                   email: new_email
                 )
               end
-              render json: { message: "退会処理が正常に完了しました。" }, status: :ok
+              render json: { message: "退会が正常に完了しました" }, status: :ok
             rescue ActiveRecord::RecordInvalid => e
-              Rails.logger.error "退会処理バリデーションエラー: #{e.record.errors.full_messages.join(', ')}"
-              render json: { error: "退会処理に失敗しました。", details: "#{e.record.errors.full_messages.join(', ')}" }, status: :internal_server_error
+              Rails.logger.error "退会バリデーションエラー: #{e.record.errors.full_messages.join(', ')}"
+              render json: { error: "退会に失敗しました", details: "#{e.record.errors.full_messages.join(', ')}" }, status: :internal_server_error
             rescue => e
-              Rails.logger.error "退会処理エラー: #{e.message}"
-              render json: { error: "退会処理に失敗しました。" }, status: :internal_server_error
+              Rails.logger.error "退会エラー: #{e.message}"
+              render json: { error: "退会に失敗しました" }, status: :internal_server_error
             end
           else
-            render json: { error: "ユーザーが見つかりません。" }, status: :not_found
+            render json: { error: "ユーザーが見つかりません" }, status: :not_found
           end
         end
       end
