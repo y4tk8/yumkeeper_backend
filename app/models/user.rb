e0cdcb02d0b3741e0a9d42aace36a8a845234a3d
Yoshitaka Ide <yoshi4363@gmail.com>
2025-05-13 20:51:48 +0900

    fix: Userモデルで定義している不要なバリデーションメッセージを削除 #143

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,12 @@ class User < ActiveRecord::Base
 
   # メールアドレスの形式チェック。英字の大文字小文字を区別しない
   VALID_EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}\z/i
-  validates :email, format: { with:    VALID_EMAIL_REGEX,
-                              message: "メールアドレスは正しい形式で入力してください" }
+  validates :email, format: { with: VALID_EMAIL_REGEX }
   before_save :downcase_email
 
   # パスワードは英字(大文字 or 小文字)と数字は必須。記号は任意。合計8文字以上。
   VALID_PASSWORD_REGEX = /\A(?=.*[A-Za-z])(?=.*\d)(?!.*\s)[A-Za-z\d!@#$%^&*\-_+=]{8,128}\z/
-  validates :password, format: { with:    VALID_PASSWORD_REGEX,
-                                 message: "パスワードは英字と数字を含んだ8文字以上にしてください" }, if: :password_required?
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, if: :password_required?
 
   validate  :validate_profile_image
   validates :username, length: { maximum: 20 }
