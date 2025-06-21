c3880ebbf9bba2acdd942a2739080c674e493bb6
Yoshitaka Ide <yoshi4363@gmail.com>
2025-06-21 17:00:23 +0900

    chore: カバレッジ計算対象からspecディレクトリを除外 #206

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,9 @@
+# テストカバレッジ算出
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/spec/"
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] = 'test'
